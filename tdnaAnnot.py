46091ff32412877a3b3310327ae828f81c9a8518
Sebastian Jaenicke <sebastian.jaenicke@computational.bio.uni-giessen.de>
2020-09-16 14:45:47 +0200

    more python3 fixes

--- tdnaAnnot.py
+++ tdnaAnnot.py
@@ -36,7 +36,7 @@ def processGFF(file):
             data = line.strip().split("\t")
             if data[2] == "gene":
                 geneID = data[8].strip().split(";")[1].split("=")[1]
-                if geneInfo.has_key(data[0]):
+                if data[0] in geneInfo:
                     geneInfo[data[0]].append((int(data[3]),int(data[4]),geneID))
                 else:
                     geneInfo[data[0]] = [(int(data[3]),int(data[4]),geneID)]
@@ -61,12 +61,12 @@ def annotateVar(varInfo,geneInfo):
     for chr,start,end in varInfo:
         #print(chr,start,end)
         flag = 0
-        if not geneInfo.has_key(chr):
+        if not chr in geneInfo:
             geneAnnot[(chr,start,end)] =[]
             continue
         for s,e,id  in geneInfo[chr]:
             if (start>= s and start <= e) or (end>=s and end <= e):
-                if geneAnnot.has_key((chr,start,end)):
+                if (chr,start,end) in geneAnnot:
                     geneAnnot[(chr,start,end)].append(id)
                 else:
                     flag = 1
